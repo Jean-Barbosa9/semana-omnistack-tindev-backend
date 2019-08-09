f5887bdbf92b0402586762333fc41dfefd390805
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-08-09 00:55:52 -0300

    Criadas rotas para buscar usuários

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -2,6 +2,47 @@ const axios = require("axios");
 const Dev = require("../models/Dev");
 
 module.exports = {
+  async index(request, response) {
+    const { filtered } = request.query;
+    const { user } = request.headers;
+    const loggedDev = await Dev.findById(user);
+
+    if (filtered && !user) {
+      return response
+        .status(400)
+        .json({ message: "An user id is necessary to filter" });
+    } else if (filtered && user) {
+      const filteredUsers = await Dev.find({
+        $and: [
+          { _id: { $ne: user } },
+          { _id: { $nin: loggedDev.likes } },
+          { _id: { $nin: loggedDev.dislikes } }
+        ]
+      });
+
+      return response.json(filteredUsers);
+    }
+
+    const devsInfos = await Dev.find();
+
+    if (!devsInfos) {
+      return response.json({ message: "There isn't any dev stored" });
+    }
+
+    return response.json(devsInfos);
+  },
+
+  async show(request, response) {
+    const { devId } = request.params;
+    const devInfos = await Dev.findById(devId);
+
+    if (!devInfos) {
+      return response.json({ message: "Developer doesn't exist" });
+    }
+
+    return response.json(devInfos);
+  },
+
   async store(request, response) {
     const { username } = request.body;
 
