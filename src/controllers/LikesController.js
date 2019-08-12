5176f0b3707f03d57f28b85404ef0b880e231402
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-08-12 00:45:39 -0300

    Emitindo evento de match para client

--- src/controllers/LikesController.js
+++ src/controllers/LikesController.js
@@ -6,6 +6,7 @@ module.exports = {
     const { devId } = request.params;
     const loggedDev = await Dev.findById(user);
     const targetDev = await Dev.findById(devId);
+    const { io, connectedUsers } = request;
 
     if (!targetDev) {
       return response
@@ -14,7 +15,16 @@ module.exports = {
     }
 
     if (targetDev.likes.includes(loggedDev._id)) {
-      console.log("We have a match");
+      const loggedSocket = connectedUsers[user];
+      const targetSocket = connectedUsers[devId];
+
+      if (loggedSocket) {
+        io.to(loggedSocket).emit("match", targetDev);
+      }
+
+      if (targetSocket) {
+        io.to(targetSocket).emit("match", loggedDev);
+      }
     }
 
     if (!loggedDev.likes.includes(targetDev._id)) {
