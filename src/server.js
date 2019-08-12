7ab5afe4273beda3aec03b30c5e27853407ce0a5
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-08-12 00:45:05 -0300

    Servindo conexão a partir do socket.io

--- src/server.js
+++ src/server.js
@@ -2,14 +2,32 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes");
-const server = express();
+
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+// TODO: Salvar essa informação no Mongo em uma nova coleção só para isso, pois o server deve ser stateless
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user } = socket.handshake.query;
+  connectedUsers[user] = socket.id;
+});
 
 const configs = require("../config");
 
 mongoose.connect(configs.mongoDBUrl, { useNewUrlParser: true });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((request, response, next) => {
+  request.io = io;
+  request.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
